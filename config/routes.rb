7429bfa065c043424e87c5dc607f823f62cbc8cc
Bion <bionjohnson@gmail.com>
2013-06-08 22:34:43 -0700

    setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 VenueCritic::Application.routes.draw do
 
+  devise_for :users, controller: { registrations: "users/registrations",
+                                   passwords: "users/passwords" }
+
   resources :venues
 
   root to: "venues#index"
