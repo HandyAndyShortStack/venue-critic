2d6f92655bf91c54dc5629bb54181a336e1a7481
Andy Short <andynshort@gmail.com>
2013-06-08 19:53:23 -0700

    testing viewing venues

---
+++ spec/integration/viewing_venues_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+feature "viewing venues" do
+  
+  let!(:venue) { Factory(:venue) }
+
+  scenario "viewing a venue" do
+
+    visit "venues"
+    click_link "Wally's"
+
+    page.should have_content("Wally's")
+    page.should have_content("down by the railroad tracks")
+    page.should have_content("Lake City")
+
+  end
+
+end
---
+++ spec/support/venue_factory.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :venue do
+    name "Wally's"
+    address "down by the railroad tracks"
+    neighborhood "Lake City"
+    alcohol false
+    allages "no"
+    stagesize "small"
+  end
+end
