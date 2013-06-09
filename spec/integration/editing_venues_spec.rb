ca51da07d2f2f2e7684679dc497aac593a3db292
Andy Short <andynshort@gmail.com>
2013-06-08 20:01:14 -0700

    testing editing venues

---
+++ spec/integration/editing_venues_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+feature "editing venues" do
+  
+  let!(:venue) { Factory(:venue) }
+
+  scenario "editing a venue" do
+    
+    visit "venues"
+    click_link "Wally's"
+    click_link "edit venue information"
+    fill_in "Allages", with: "yes"
+    click_button "Update Venue"
+
+    page.should have_content "Venue information updated"
+    page.should have_content "yes"
+
+  end
+
+end
